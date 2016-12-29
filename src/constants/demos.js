6c73b4167c4dc58a4e29c5db36e972968de85dc8
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-12-29 14:58:34 +0100

    Add Demo2

--- src/constants/demos.js
+++ src/constants/demos.js
@@ -16,4 +16,20 @@ export default [
       ALL_LINKS: 'https://d3ykzhoetxh1w.cloudfront.net/botify-galaxy-demo1-links.csv',
     },
   },
+  {
+    id: 91558,
+    owner: 'Botify',
+    projectSlug: 'Galaxy',
+    analysisSlug: 'Demo2',
+    env: 'production',
+    crawledUrls: 135969,
+    knownUrls: 135969,
+    links: 12506098,
+    extractsName: ['Breadcrumb'],
+    segmentsName: ['PageType'],
+    exports: {
+      ALL_URL_DETAILS: 'https://d3ykzhoetxh1w.cloudfront.net/botify-galaxy-demo2-urls.csv',
+      ALL_LINKS: 'https://d3ykzhoetxh1w.cloudfront.net/botify-galaxy-demo2-links.csv',
+    },
+  }
 ];
